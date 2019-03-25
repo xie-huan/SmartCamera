817a21b29c21be85f37880de82bbff811c359a4a
xx790008409 <790008409@qq.com>
2019-03-25 14:02:59 +0800

    fixed a bug

--- app/src/main/java/com/example/a79000/smartcamera/SavePicture.java
+++ app/src/main/java/com/example/a79000/smartcamera/SavePicture.java
@@ -27,7 +27,6 @@ public class SavePicture {
     save the image to gallery
      */
     public static void saveImageToGallery(Bitmap bmp,String bitName ) {
-        // 首先保存图片
         File appDir = new File(Environment.getExternalStorageDirectory(),"SmartCamera");
         if (!appDir.exists()) {
             appDir.mkdir();
@@ -58,22 +57,17 @@ public class SavePicture {
     public static boolean saveBmp2Gallery(Context context, Bitmap bmp, String picName) {
         //saveImageToGallery(bmp,picName);
         String fileName = null;
-        //系统相册目录
+        //dir
         String galleryPath = Environment.getExternalStorageDirectory()
                 + File.separator + Environment.DIRECTORY_DCIM
                 + File.separator + "Camera" + File.separator;
 
-
-        // 声明文件对象
         File file = null;
-        // 声明输出流
+        // output stream
         FileOutputStream outStream = null;
         try {
-            // 如果有目标文件，直接获得文件对象，否则创建一个以filename为名称的文件
             file = new File(galleryPath, picName + ".jpg");
-            // 获得文件相对路径
             fileName = file.toString();
-            // 获得输出流，如果文件中有内容，追加内容
             outStream = new FileOutputStream(fileName);
             if (null != outStream) {
                 bmp.compress(Bitmap.CompressFormat.JPEG, 90, outStream);
@@ -91,7 +85,7 @@ public class SavePicture {
         }
 
         //broadcast
-        MediaStore.Images.Media.insertImage(context.getContentResolver(),bmp,fileName,null);
+        //MediaStore.Images.Media.insertImage(context.getContentResolver(),bmp,fileName,null);
         Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
         Uri uri = Uri.fromFile(file);
         intent.setData(uri);
