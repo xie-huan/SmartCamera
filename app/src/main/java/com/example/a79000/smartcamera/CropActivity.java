f8e2876096c13e1fa5b9d2e55f2fc9d01875d532
xx790008409 <790008409@qq.com>
2019-03-26 09:17:56 +0800

    problem sloved: save two same pic

--- app/src/main/java/com/example/a79000/smartcamera/CropActivity.java
+++ app/src/main/java/com/example/a79000/smartcamera/CropActivity.java
@@ -1,6 +1,6 @@
 package com.example.a79000.smartcamera;
 
-import android.Manifest;
+
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
@@ -8,10 +8,8 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Rect;
 import android.net.Uri;
-import android.os.Build;
 import android.os.StrictMode;
 import android.provider.MediaStore;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
